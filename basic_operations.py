1b5a99407e106efb4fa0fe0fe824d72b2b56db1a
edwineduuuh <edumwas92@gmail.com>
2025-12-30 21:16:46 +0300

    Basic Arithmetic Exercise

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+# Task Description:
+
+# You are required to complete a Python script that performs basic arithmetic operations with two predefined numbers. 
+# The script should do the following:
+
+# Assign specific values to two variables, number1 and number2.
+# Perform addition, subtraction, and multiplication on these numbers.
+# Print the results of these operations in a human-readable format.
+number1 = 10
+number2 = 5
+
+print(f"Addition of 10 and 5 is {number1 + number2}")
+print(f"Subtraction of 10 and 5 is {number1 - number2}")
+print(f"Multiplication of 10 and 5 is {number1 * number2}")
+
