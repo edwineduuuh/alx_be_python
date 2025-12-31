8780caecec069e28a54cedd79b3becf33d268770
syntax-elpadrino <eduardoeduuh@proton.me>
2025-12-31 22:37:03 +0300

    Multiplication table generator

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -9,10 +9,15 @@
 # (the current number in the loop from 1 to 10).
 # Print each line of the multiplication table in the format: “X * Y = Z”, 
 # where X is the user’s number, Y is the current number in the loop, and Z is the product.
+# number = int(input("Enter a number to see its multiplication table: "))
+
+# for i in range(1,11):
+#     for j in range(1,number + 1):
+#         product = j * i
+#     print(f"{j} * {i} = {product}", end="\n")
+# print()
+
 number = int(input("Enter a number to see its multiplication table: "))
 
-for i in range(1,11):
-    for j in range(1,number + 1):
-        product = j * i
-    print(f"{j} * {i} = {product}", end="\n")
-print()
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
