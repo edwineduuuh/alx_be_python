d9fd5a60fdb111b87eda375add569061972d2a44
syntax-elpadrino <eduardoeduuh@proton.me>
2025-12-31 22:11:59 +0300

    Weather recommedation

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -16,12 +16,14 @@
 
 # Print the clothing recommendation based on the weather condition provided by the user.
 
+
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
+
 if weather == 'sunny':
     print("Wear a t-shirt and sunglasses.")
-elif weather == 'rainy':
+elif weather == "rainy":
     print("Don't forget your umbrella and raincoat.")
 elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
-else:
+else: 
     print("Sorry, I don't have recommendations for this weather.")
