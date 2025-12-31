e3af05d7dca68028dceb6141dbed8fe988c60f4f
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-01 00:27:11 +0300

    Simple Calculator with Match Case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,39 @@
+# Instructions:
+# Prompt for User Input:
+# Ask the user to input two numbers (one at a time) using: Enter the first number: 
+# and Enter the second number:
+# Make sure you use num1 and num2 for first and second numbers
+# Ask for the type of operation they’d like to perform: Choose the operation 
+# (+, -, *, /):.
+# Perform the Calculation Using Match Case:
+# Implement a Match Case statement that executes the chosen operation based on the 
+# user’s input.
+# For addition (+), subtract (-), multiply (*), and divide (/).
+# Ensure to handle the division by zero case gracefully, displaying
+#  a message if the user tries to divide by zero.
+# Output the Result:
+# Display the result of the operation in a user-friendly message, e.g.,
+#  The result is [result].
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+operation = input("Choose the operation (+, -, *, /):")
+match operation:
+    case '+':
+        result = num1 + num2
+        print(f"The result is {result}")
+    case '-':
+        result = num1 - num2
+        print(f"The result is {result}")
+    case '*':
+        result =num1 * num2
+        print(f"The result is {result}")
+    case '/':
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}")
+    case _:
+        print("Please use valid operations")
