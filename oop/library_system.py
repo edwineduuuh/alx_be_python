91aafa3f2b6e20d8d8da2aac5a5c5741fcd5f662
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-09 14:20:55 +0300

    Library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -22,7 +22,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count} pages"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
